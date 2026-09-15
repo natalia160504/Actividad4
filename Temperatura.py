568f7ac4a31250f6530ef615636a7591863d0617
natalia160504 <a395063@uach.mx>
2026-09-15 09:38:56 -0600

    Format number input for temperature entry

--- Temperatura.py
+++ Temperatura.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.title("Clasificador de temperatura")
 
-temperatura= st.number_input("Introduce la temperatura en °c:", value=20)
+temperatura= st.number_input(
+  "Introduce la temperatura en °c:",
+  value=20
+                            )
 
 if temperatura <10:
   st.write("Hace frio")
