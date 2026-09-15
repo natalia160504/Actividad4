094ca737238605e70b557927519698058f933d71
natalia160504 <a395063@uach.mx>
2026-09-15 09:34:05 -0600

    Fix temperature input variable and add conditions

--- Temperatura.py
+++ Temperatura.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title("Clasificador de temperatura")
 
-Temperatura= st.number_input("Introduce la temperatura en c:", value=20)
+temperatura= st.number_input("Introduce la temperatura en °c:", value=20)
+
+if temperatura <10:
+  st.write("Hace frio")
+elif temperatura >=10 and temperatura <=24:
+  st.write("La temperatura es agradable")
+  else
+  st.write("Hace calor")
